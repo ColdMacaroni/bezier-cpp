e5b3b83ec382aa748946727d7a343928418de422
muffin <diegoxe7117@gmail.com>
2021-09-18 19:11:25 +1200

    require opengl and pt input func

--- src/main.cpp
+++ src/main.cpp
@@ -1,19 +1,74 @@
 #include <iostream>
+#include <vector>
+#include <string>
+#include <cctype>
+
+#include <GL/freeglut.h>
+
 #include <point.hpp>
 
 typedef point::Point<double> Point;
 
-int main( int argc, char** argv)
+auto
+string_to_pair(std::string str)
 {
+    double x{};
+    double y{};
+    double *cur = &x;
 
+    size_t off = 0;
 
-    return 0;
+    for (size_t i = 0; i < str.size(); i++)
+        if (std::isdigit(str[i]))
+        {
+            // Convert to num
+            *cur = std::stod(str.substr(i, str.size()), &off);
+
+            // Skip the whole converted num
+            i += off;
+
+            // now eval the next num
+            cur = &y;
+        }
+
+    return std::make_pair(x, y);
 }
 
-auto
-str_xy(std::string &str)
+/* glut */
+void init()
 {
-    /* https://old.reddit.com/r/learncpp/comments/pinj4p/someone_told_me_my_code_looks_like_c_am_i_taking/hbrb8vs/ */
-    auto const comma = str.find(',');
-    return std::make_pair( strtod(str.c_str(),nullptr), strtod(str.c_str()+comma+1,nullptr));
+	glutInitDisplayMode(GLUT_SINGLE|GLUT_RGB);	//Line C
+	glutInitWindowSize(640,480);
+    glutInitWindowPosition(1000,200);
+    glutCreateWindow("Bezier");
+}
+
+void display()
+{
+    glClearColor(1.0,1.0,1.0,0.0);
+	glClear(GL_COLOR_BUFFER_BIT);
+	// gluOrtho2D(0.0,100.0,0,100.0);
+	glFlush();
+}
+
+/**/
+
+int main( int argc, char** argv)
+{
+    std::vector<Point> points;
+    std::string input = "";
+
+    // Get points until EOF, separated by newline
+    while (std::getline(std::cin, input))
+        points.push_back(Point(string_to_pair(input)));
+
+    for (long unsigned i = 0; i < points.size(); i++)
+        std::cout << points[i].to_string() << '\n';
+
+    glutInit(&argc,argv);			//Line A
+        init();					//Line B
+        glutDisplayFunc(display);
+        glutMainLoop();
+
+    return 0;
 }
